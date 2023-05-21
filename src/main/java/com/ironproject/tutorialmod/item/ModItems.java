658cc786600c52999ad0faf94572ef98262800c5
IronBrandon <ironbrandon22@gmail.com>
2023-05-21 16:14:07 -0500

    Finished Fifth Tutorial.
    Added from Tutorial: Eight Ball.

--- src/main/java/com/ironproject/tutorialmod/item/ModItems.java
+++ src/main/java/com/ironproject/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.ironproject.tutorialmod.item;
 
 import com.ironproject.tutorialmod.TutorialMod;
+import com.ironproject.tutorialmod.item.custom.EightBallItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,8 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).stacksTo(1)));
     public static final RegistryObject<Item> OWPOW_GEMS = ITEMS.register("owpow_gems",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.OWPOW_TAB)));
 
